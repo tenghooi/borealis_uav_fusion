e48764bab7319cbcf0ea100862fbc41b9e3238be
TengHooi <chantenghooi@gmail.com>
2022-06-21 16:04:39 +0800

    modify CMakeLists; expand ekf_node.cpp

--- src/ekf_node.cpp
+++ src/ekf_node.cpp
@@ -4,10 +4,10 @@
                    
 int main(int argc, char** argv)
 {           
+    ros::init(argc, argv, "uav_fusion");
+    ros::NodeHandle node("~");
 
-
-
-
+    ros::spin();
 
     return 0;
 }
