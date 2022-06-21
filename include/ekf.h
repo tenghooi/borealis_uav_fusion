0cd8ad1e7f89e9267003adbe841f0f02804e9001
TengHooi <chantenghooi@gmail.com>
2022-06-21 15:33:53 +0800

    add main func

--- include/ekf.h
+++ include/ekf.h
@@ -1,9 +1,15 @@
 #ifndef _BOREALIS_EKF_H_
 #define _BOREALIS_EKF_H_
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
 
+class ExtendedKalmanFilter
+{
 
-
+};
 
 
 #endif // _BOREALIS_EKF_H_
---
+++ src/ekf_node.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <ros/ros.h>                   
+                   
+int main(int argc, char** argv)
+{           
+
+
+
+
+
+    return 0;
+}
