fcf3fdee6bfd9db0a352e7381a9adf5354d20879
TengHooi <chantenghooi@gmail.com>
2022-06-28 15:52:18 +0800

    modify CMakeLists; modify kalman_filter.h

---
+++ include/kalman_filter.h
@@ -0,0 +1,15 @@
+#ifndef _BOREALIS_KALMAN_FILTER_H_
+#define _BOREALIS_KALMAN_FILTER_H_
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+
+#include <ros/ros.h>
+
+#include <Eigen/Dense>
+
+
+
+#endif // _BOREALIS_KALMAN_FILTER_H_
