70567c80d0bc175e62fb6a63debaf781c1260b50
TengHooi <chantenghooi@gmail.com>
2022-06-30 18:18:45 +0800

    added class members to KalmanFilter

--- include/kalman_filter.h
+++ include/kalman_filter.h
@@ -12,10 +12,20 @@
 
 class KalmanFilter
 {
-private:
-
 protected:
+    Eigen::MatrixXd state_transition_F;
+    Eigen::MatrixXd control_transition_B;
+    Eigen::MatrixXd observation_H;
 
+    Eigen::VectorXd states_x;
+    Eigen::VectorXd control_u;
+    Eigen::VectorXd measurement_z;
+
+    Eigen::MatrixXd states_cov_P;
+    Eigen::MatrixXd process_noise_cov_Q;
+    Eigen::MatrixXd measurement_noise_cov_R;
+
+    
 public:
 
 
