6600ca7bf0196e24ab962b4cbbe9b7be17321d4e
Chan Teng Hooi <chantenghooi@gmail.com>
2022-07-04 04:02:40 +0800

    create filter_server class; modify CMakeLists

--- include/filter_server.h
+++ include/filter_server.h
@@ -1,12 +1,40 @@
-#ifndef _BOREALIS_EKF_SERVER_H_
-#define _BOREALIS_EKF_SERVER_H_
+#ifndef _BOREALIS_FILTER_SERVER_H_
+#define _BOREALIS_FILTER_SERVER_H_
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+
+#include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <sensor_msgs/Imu.h>
+
+#include <Eigen/Dense>
+
+#include "node_param.h"
+#include "update_handler.h"
+#include "kalman_filter.h"
 #include "ekf.h"
 
-class EKFServer
-{
 
+class FilterServer
+{
+private:
+    NodeParams parameters_;
+
+    ros::Subscriber lidar_sub_;
+    ros::Subscriber imu_sub_;
+
+    ros::Publisher fused_pose_pub_;
+
+public:
+    FilterServer(ros::NodeHandle node);
+    ~FilterServer();
+
+    void PoseCallBack(const nav_msgs::OdometryConstPtr& measurement_msg);
+    void ControlCallBack(const sensor_msgs::ImuConstPtr& control_msg);
 };
 
-
-#endif // _BOREALIS_EKF_SERVER_H_
+#endif // _BOREALIS_FILTER_SERVER_H_
