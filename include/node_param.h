7f4c923495251e4cf1ff051cb7233bded5b0d99a
Chan Teng Hooi <chantenghooi@gmail.com>
2022-07-04 03:43:12 +0800

    create NodeParams class

--- include/node_param.h
+++ include/node_param.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
@@ -10,11 +11,20 @@
 
 #include <Eigen/Dense>
 
+class NodeParams
+{
+public:
+    uint n_states_;
+    uint n_measurements_;
+  
+    Eigen::MatrixXd process_transition_F_;
+    Eigen::MatrixXd control_transition_B_;
+    Eigen::MatrixXd observation_transition_H_;
 
-
-
-
-
-
+    Eigen::MatrixXd initial_states_cov_P_;
+    Eigen::MatrixXd sensor_noise_cov_R_;
+    Eigen::MatrixXd process_noise_cov_Q_;
+    
+};
 
 #endif // _BOREALIS_FUSION_NODE_PARAM_H_
