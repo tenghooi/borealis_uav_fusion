d5b70a8c9af50db1b050b0566616c0faa93e0f6a
TengHooi <chantenghooi@gmail.com>
2022-07-04 20:51:49 +0800

    node_param: define and implement; create params.yaml

--- src/node_param.cpp
+++ src/node_param.cpp
@@ -10,7 +10,13 @@ void NodeParams::SetNodeParams(const ros::NodeHandle& node)
     node.param<int>("n_control_inputs", n_control_inputs_, 6);
     node.param<int>("n_measurement_states", n_measurement_states_, 3);
 
+    node.param<std::vector<double>>("borealis_fusion/process_transition_F", F_vector_form_, std::vector<double>());
+    node.param<std::vector<double>>("borealis_fusion/control_transition_B", B_vector_form_, std::vector<double>());
+    node.param<std::vector<double>>("borealis_fusion/observation_transition_H", H_vector_form_, std::vector<double>());
 
+    node.param<std::vector<double>>("borealis_fusion/initial_states_cov_P", P_vector_form_, std::vector<double>());
+    node.param<std::vector<double>>("borealis_fusion/process_noise_cov_Q", Q_vector_form_, std::vector<double>());
+    node.param<std::vector<double>>("borealis_fusion/sensor_noise_cov_R", R_vector_form_, std::vector<double>());
 
 
 }
