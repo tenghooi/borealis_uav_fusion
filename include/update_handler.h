956c7098ec363ac2d6c63835285b29a1efd689f4
TengHooi <chantenghooi@gmail.com>
2022-07-07 11:43:12 +0800

    create update_handler files; modify CMakeLists

---
+++ include/update_handler.h
@@ -0,0 +1,16 @@
+#ifndef _BOREALIS_FUSION_UPDATE_HANDLER_H_
+#define _BOREALIS_FUSION_UPDATE_HANDLER_H_
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+
+class UpdateHandler
+{
+
+};
+
+
+
+#endif // _BOREALIS_FUSION_UPDATE_HANDLER_H_
---
+++ src/update_handler.cpp
@@ -0,0 +1,1 @@
+#include "update_handler.h"
