2b11ed44b2b71dc45f36fdd5c280378bb99e2129
TengHooi <chantenghooi@gmail.com>
2022-09-01 13:57:48 +0800

    State: implement ConverToPoseMsg()

--- src/state.cpp
+++ src/state.cpp
@@ -17,5 +17,17 @@ void State::setStateTime(std_msgs::Header& header)
 
 void State::ConvertToPoseMsg(geometry_msgs::PoseWithCovarianceStamped& pose_msg)
 {
-    
+    ros::Time time(state_time_);
+    pose_msg.header.stamp = time;
+    pose_msg.header.frame_id = "camera_init";
+
+    pose_msg.pose.pose.position.x = position_[0];
+    pose_msg.pose.pose.position.y = position_[1];
+    pose_msg.pose.pose.position.z = position_[2];
+
+    pose_msg.pose.pose.orientation.w = attitude_.w();
+    pose_msg.pose.pose.orientation.x = attitude_.x();
+    pose_msg.pose.pose.orientation.y = attitude_.y();
+    pose_msg.pose.pose.orientation.z = attitude_.z();
+
 }
