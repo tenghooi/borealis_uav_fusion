dd560075f572e898864355622691c968512f207a
TengHooi <chantenghooi@gmail.com>
2022-09-06 19:31:23 +0800

    modify filter_node.cpp

--- src/filter_node.cpp
+++ src/filter_node.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 
-#include <ros/ros.h>                   
+#include <ros/ros.h>     
+
+#include "filter_server.h"
                    
 int main(int argc, char** argv)
 {           
+    std::cout << "gg" <<std::endl;
     ros::init(argc, argv, "uav_fusion");
     ros::NodeHandle node("~");
+    std::cout << "gg1" <<std::endl;
+    FilterServer filter_server(node);
 
     ros::spin();
 
